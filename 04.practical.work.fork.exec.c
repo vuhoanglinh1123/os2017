c8ba78d197bd4b417b855149bd01c78d47d74be9
vhlinh <vuhoanglinh1997@gmail.com>
2017-02-02 18:17:59 +0700

    Fix the text format of Report 4

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -7,31 +7,31 @@
 int main() {
 	printf("Main before fork()\n");
 	
-	int pid_1 = fork();											//First fork()
+	int pid_1 = fork();							//First fork()
 
 	printf("\npid = %d\n", pid_1);
 
-	if (pid_1 == 0) {											//Child no.1
+	if (pid_1 == 0) {							//Child no.1
 																
-		int pid_2 = fork();										//Second fork() inside Child no.1
+		int pid_2 = fork();						//Second fork() inside Child no.1
 
 		printf("\npid = %d\n", pid_2);
 
-		if (pid_2 == 0) {										//Child no.2
+		if (pid_2 == 0) {						//Child no.2
 
-			printf("\nLaunching free -h\n");					//Executing the second command
+			printf("\nLaunching free -h\n");			//Executing the second command
 			char *args[] = {"/usr/bin/free","-h",NULL};
 			execvp("/usr/bin/free",args);
 		}
-		else {													//Parent no.2 inside Child no.1	
+		else {								//Parent no.2 inside Child no.1	
 			printf("Parent of Child with PID = %d\n",pid_2);
 		}
 
-		printf("\nLaunching ps -ef\n");							//Executing the first command
+		printf("\nLaunching ps -ef\n");					//Executing the first command
 		char *args[] = {"/bin/ps","-ef",NULL};
 		execvp("/bin/ps",args);
 	}
-	else {														//Parent no.1
+	else {									//Parent no.1
 		printf("Parent of Child with PID = %d\n", pid_1);
 	}
 
