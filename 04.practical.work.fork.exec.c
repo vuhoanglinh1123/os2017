a59607b76ff996c77d9c9d829e86536e18dca7d2
vuhoanglinh1123 <vuhoanglinh1997@gmail.com>
2017-02-02 20:52:55 +0700

    Correcting mistakes of Report 4
    Make the second fork() branches off the previous Parent instead of the Child

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -13,26 +13,26 @@ int main() {
 
 	if (pid_1 == 0) {							//Child no.1
 																
-		int pid_2 = fork();						//Second fork() inside Child no.1
+		printf("\nLaunching free -h\n");				//Executing the second command
+		char *args[] = {"/usr/bin/free","-h",NULL};
+		execvp("/usr/bin/free",args);
+	}
+	else {									//Parent no.1
+		printf("Parent of Child with PID = %d\n", pid_1);
+
+		int pid_2 = fork();						//Second fork() inside Parent no.1
 
 		printf("\npid = %d\n", pid_2);
 
 		if (pid_2 == 0) {						//Child no.2
 
-			printf("\nLaunching free -h\n");			//Executing the second command
-			char *args[] = {"/usr/bin/free","-h",NULL};
-			execvp("/usr/bin/free",args);
+			printf("\nLaunching ps -ef\n");				//Executing the first command
+			char *args[] = {"/bin/ps","-ef",NULL};
+			execvp("/bin/ps",args);
 		}
-		else {								//Parent no.2 inside Child no.1	
+		else {								//Parent no.2 inside Parent no.1	
 			printf("Parent of Child with PID = %d\n",pid_2);
 		}
-
-		printf("\nLaunching ps -ef\n");					//Executing the first command
-		char *args[] = {"/bin/ps","-ef",NULL};
-		execvp("/bin/ps",args);
-	}
-	else {									//Parent no.1
-		printf("Parent of Child with PID = %d\n", pid_1);
 	}
 
 	return 0;
